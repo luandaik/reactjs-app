bf23a939b0922ccda144a1b4eb7fd15d28647d79
DoanLuan <vebinh2014@gmail.com>
2021-12-14 15:45:40 +0700

    post feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import Clock from "./features/Clock";
 import Post from "./features/Post";
 function App() {
   return (
     <div className="App">
-     {/* <Post /> */}
-     <Clock />
+     <Post />
     </div>
   );
 }
