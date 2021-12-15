2b305540ad92f6b578c244b18d3119ecdc3dff30
DoanLuan <vebinh2014@gmail.com>
2021-12-15 16:29:58 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { NavLink, Route, Switch } from "react-router-dom";
 import AllbumFeature from "./features/Allbum";
 import Clock from "./features/Clock";
 import Counter from "./features/Counter";
@@ -9,12 +9,18 @@ import TodoFeature from "./features/Todo";
 function App() {
   return (
     <div className="App">
-      <Route path="/posts" component={Post} />
-      <Route path="/todo" component={TodoFeature} />
-      <Route path="/magiccolor" component={MagicColor} />
-      <Route path="/counter" component={Counter} />
-      <Route path="/clock" component={Clock} />
-      <Route path="/allbum" component={AllbumFeature} />
+
+      <p> <NavLink to='posts' >Post</NavLink></p>
+      <p> <NavLink to='todo' activeClassName="active-menu" >Todo</NavLink></p>
+      <Switch>
+        <Route path="/posts" component={Post} />
+        <Route path="/todo" component={TodoFeature} />
+        <Route path="/magiccolor" component={MagicColor} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/clock" component={Clock} />
+        <Route path="/allbum" component={AllbumFeature} />
+      </Switch>
+
     </div>
   );
 }
